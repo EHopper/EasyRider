a3965b383fcd5d5b2a180083e4e3aacc1920f79c
Emily Hopper <emily@ehopper.fyi>
2020-09-20 21:27:41 -0400

    Setup package directory

---
+++ view_finder/setup.py
@@ -0,0 +1,5 @@
+
+from setuptools import setup, find_packages
+
+set(name="view_finder",
+    version="0.1")
---
+++ view_finder/view_finder/config.py
@@ -0,0 +1,6 @@
+# config.py
+
+from pathlib import Path
+
+
+data_dir = Path('../data/')
